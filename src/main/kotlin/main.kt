1c7576a3760837dddfb0e622414294860740b9c4
Dino Oliva <dino@docdino.com>
2021-06-30 21:20:59 -0700

    Remove 'Example' from class names.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,54 +1,54 @@
 fun main(args: Array<String>) {
-    runClassExample()
-    runConstructorExample()
-    runNestedClassExample()
-    runNestedStaticClassExample()
-    runAnonymousClassExample()
-    runLocalClassExample()
-    runAbstractClassExample()
-    runInterfaceExample()
-    runEnumExample()
+    runClass()
+    runConstructor()
+    runNestedClass()
+    runNestedStaticClass()
+    runAnonymousClass()
+    runLocalClass()
+    runAbstractClass()
+    runInterface()
+    runEnum()
 }
 
-fun runClassExample() {
+fun runClass() {
     System.err.println("Class (J): " + ClassJa(5).getExampleI())
     System.err.println("Class (K): " + ClassKt(5).getExampleI())
 }
 
-fun runConstructorExample() {
+fun runConstructor() {
     System.err.println("Constructor (J): " + ConstructorKt().x + " : " + ConstructorKt(5).x)
     System.err.println("Constructor (K): " + ConstructorKt().x + " : " + ConstructorKt(5).x)
 }
 
-fun runNestedClassExample() {
+fun runNestedClass() {
     val ncJ = NestedClassJa(6).Nested()
     System.err.println("Nested Class (J): " + ncJ.getExampleI())
     val ncK = NestedClassKt(6).Nested()
     System.err.println("Nested Class (K): " + ncK.getExampleI())
 }
 
-fun runNestedStaticClassExample() {
+fun runNestedStaticClass() {
     val sncJ = NestedStaticClassJa.Nested(7)
     System.err.println("Static Nested Class (J): " + sncJ.getExampleI())
     val sncK = NestedStaticClassKt.Nested(7)
     System.err.println("Static Nested Class (K): " + sncK.getExampleI())
 }
 
-fun runAnonymousClassExample() {
+fun runAnonymousClass() {
     val acJ = AnonymousClassJa(8).getB()
     System.err.println("Anonymous Class (J): " + acJ.getValue())
     val acK = AnonymousClassJa(8).getB()
     System.err.println("Anonymous Class (K): " + acK.getValue())
 }
 
-fun runLocalClassExample() {
+fun runLocalClass() {
     val acJ = LocalClassJa(9).getB()
     System.err.println("Local Class (J): " + acJ.getValue())
     val acK = LocalClassKt(9).getB()
     System.err.println("Local Class (K): " + acK.getValue())
 }
 
-fun runAbstractClassExample() {
+fun runAbstractClass() {
     val acJ = AbstractClassJa.A()
     System.err.println(
         "Abstract Class (J): " + " : " + acJ.getAbstractClassValue() + " : " + acJ.getAbstractClassValueX2()
@@ -59,14 +59,14 @@ fun runAbstractClassExample() {
     )
 }
 
-fun runInterfaceExample() {
+fun runInterface() {
     val iJ = InterfaceJa.OneF()
     System.err.println("Interface (J): " + iJ.f())
     val iK = InterfaceJa.OneF()
     System.err.println("Interface (K): " + iK.f())
 }
 
-fun runEnumExample() {
+fun runEnum() {
     for (eJ in EnumJa.values()) {
         System.err.println("Enum (J): " + eJ.ordinal + " : " + eJ.getEnumValue() + " : " + eJ.getEnumValueX2())
     }
