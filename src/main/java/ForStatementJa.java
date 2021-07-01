cf091d88e63f1be391fdeb2e203aa58d93cc58f3
Dino Oliva <dino@docdino.com>
2021-07-01 13:14:45 -0700

    Add control flow examples for 'if' and 'for'.

---
+++ src/main/java/ForStatementJa.java
@@ -0,0 +1,33 @@
+public class ForStatementJa {
+    int loop(int n) {
+        int i = 0;
+        for (int j = 0; j < n; j++) {
+            i++;
+        }
+        return i;
+    }
+
+    int loopWithBreak(int n) {
+        int i = 0;
+        int j;
+        out: for (j = 0; j < n; j++) {
+            if (j > n/2) {
+                break out;
+            }
+            i++;
+        }
+        return j == n ? -1 : i;
+    }
+
+    int loopWithContinue(int n) {
+        int i = 0;
+        int j;
+        out: for (j = 0; j < n; j++) {
+            if (j > n / 2) {
+                continue out;
+            }
+            i++;
+        }
+        return j == n ? i : -1;
+    }
+}
