29ddb068daee4713b1248e18f11623002ce6b7a6
Dino Oliva <dino@docdino.com>
2021-07-02 09:18:34 -0700

    Add control flow examples for 'if' and 'for'.

---
+++ src/main/kotlin/ForStatementKt.kt
@@ -0,0 +1,38 @@
+class ForStatementKt {
+    fun loop(n: Int): Int {
+        var i = 0
+        var j = 0
+        while (j < n) {
+            i++
+            j++
+        }
+        return i
+    }
+
+    fun loopWithBreak(n: Int): Int {
+        var i = 0
+        var j = 0
+        out@ while (j < n) {
+            if (j > n / 2) {
+                break@out
+            }
+            i++
+            j++
+        }
+        return if (j == n) -1 else i
+    }
+
+    fun loopWithContinue(n: Int): Int {
+        var i = 0
+        var j = 0
+        out@ while (j < n) {
+            if (j > n / 2) {
+                j++
+                continue@out
+            }
+            i++
+            j++
+        }
+        return if (j == n) i else -1
+    }
+}
