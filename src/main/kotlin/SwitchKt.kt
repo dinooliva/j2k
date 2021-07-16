09adddc686f4fb97b3d5d1be29ca4298757c3f6f
Dino Oliva <dino@docdino.com>
2021-07-16 14:46:27 -0700

    Add Switch/When example.

--- src/main/kotlin/SwitchKt.kt
+++ src/main/kotlin/SwitchKt.kt
@@ -1,19 +1,19 @@
 class SwitchKt {
     fun basic(i: Int): Int {
         var retval = 0
-        retval += when (i) {
-            1 -> 10
-            2 -> 20
-            else -> i * 100
+        when (i) {
+            1 -> retval += 10
+            2 -> retval += 20
+            else -> retval += i * 100
         }
         return retval
     }
 
     fun simpleFallThrough(i: Int): Int {
         var retval = 0
-        retval += when (i) {
-            1, 2 -> 20
-            else -> i * 100
+        when (i) {
+            1, 2 -> retval += 20
+            else -> retval += i * 100
         }
         return retval
     }
