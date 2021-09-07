1791f5269370f0461ea325d89063ac2c5f739bd2
Dino Oliva <dino@docdino.com>
2021-09-07 14:31:23 -0700

    Add examples for primary and secondary constructors.

--- src/main/java/ConstructorJa.java
+++
@@ -1,9 +0,0 @@
-public class ConstructorJa {
-    final int x;
-
-    ConstructorJa(int x) {
-        this.x = x;
-    }
-
-    ConstructorJa() { this(42); }
-}
--- src/main/kotlin/ConstructorKt.kt
+++
@@ -1,9 +0,0 @@
-class ConstructorKt {
-    val x: Int
-
-    constructor(x: Int) {
-        this.x = x
-    }
-
-    constructor() : this(42)
-}
